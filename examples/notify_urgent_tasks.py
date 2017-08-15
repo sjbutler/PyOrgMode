a4b682dbd1bb41635206a66e5f083a8045302001
dionisos <denis.baudouin@gmail.com>
2017-08-15 22:37:16 +0200

    Add notify_urgent_tasks as a usage example

---
+++ examples/notify_urgent_tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+import sys
+import pynotify
+sys.path.append('../')
+
+
+def sendmessage(title, message):
+    pynotify.init("Test")
+    notice = pynotify.Notification(title, message)
+    notice.show()
+    return
+
+from PyOrgMode import PyOrgMode
+from datetime import date
+
+base = PyOrgMode.OrgDataStructure()
+base.load_from_file("/home/dionisos/organisation/agenda.org")
+
+urgent_tasks = []
+for task in base.extract_todo_list():
+    if (task.priority == 'A'):
+        if hasattr(task, "scheduled") and task.scheduled and task.scheduled.should_be_done():
+            urgent_tasks.append(task)
+
+if len(urgent_tasks) != 0:
+    message = ""
+    for task in urgent_tasks:
+        message += str(task) + "\n"
+    sendmessage('You have some urgent tasks !!!', message)
